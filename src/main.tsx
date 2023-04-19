89a25604c82cf40610142460311b29b6bafc1eae
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-04-18 21:39:39 -0300

    feat: create redirect route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/globals.css'
 import { ThemeProvider } from './contexts/theme'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+  useParams
+} from 'react-router-dom'
+import './styles/globals.css'
+
+function RedirectPage() {
+  const { id } = useParams()
+
+  useEffect(() => {
+    window.location.href = `${import.meta.env.VITE_API_URL}/api/v1/link/${id}`
+  }, [])
+
+  return <></>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/reducely/",
+    element: <App />,
+  },
+  {
+    path: "/reducely/:id",
+    element: <RedirectPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 )
